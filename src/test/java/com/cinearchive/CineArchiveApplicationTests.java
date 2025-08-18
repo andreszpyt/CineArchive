ff5f86c612686fed72c3cb9da23c60bdb321f736
andre-szp <andre.szp@outlook.com>
2025-08-18 19:15:24 -0300

    first commit

---
+++ src/test/java/com/cinearchive/CineArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinearchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
