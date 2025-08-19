5ad56268e2f6ef492dfc6f6557a77e8712ec9c44
andre-szp <andre.szp@outlook.com>
2025-08-18 23:26:58 -0300

    feat: created the basic services

--- src/main/java/com/cinearchive/repository/StreamingRespository.java
+++ src/main/java/com/cinearchive/repository/StreamingRespository.java
@@ -1,6 +1,8 @@
 package com.cinearchive.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StreamingRespository extends JpaRepository<StreamingRespository, Long> {
 }
---
+++ src/main/java/com/cinearchive/service/StreamingService.java
@@ -0,0 +1,10 @@
+package com.cinearchive.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StreamingService {
+    private final StreamingService streamingService;
+}
